92c3d720b96cd2ad5c98be0ba5289c3e4906ab06
Erik Seidel <erik@znsl.us>
2023-03-26 11:08:00 +0000

    marshmallow validation migration part 3

--- models/netdb_policy.py
+++ models/netdb_policy.py
@@ -1,6 +1,6 @@
 
 from .netdb_column      import netdbColumn
-#from .netdb_device      import netdbDevice
+from schema.policy      import *
 
 class netdbPolicy(netdbColumn):
 
@@ -22,39 +22,9 @@ class netdbPolicy(netdbColumn):
                 if 'roles' not in categories.keys():
                     return { 'result': False, 'comment': "%s: roles required for shared config set" % top_id }
 
-            for category, contents in categories.items():
-                if category in ['prefix_lists', 'route_maps']:
-                    for family, items in contents.items():
-                        if family not in ['ipv4', 'ipv6']:
-                            return { 'result': False, 'comment': "%s.%s: family must me either ipv4 or ipv6" % (top_id, category) }
-                        for element, elem_data in items.items():
-                            if 'rules' not in elem_data:
-                                return { 'result': False, 'comment': "%s.%s.%s: only rules element should be set" % (top_id, category, element) }
-                            for rule in elem_data['rules']:
-                                # Needs to be a schema
-                                for key in rule.keys():
-                                    if category == 'route_maps':
-                                        if key not in ['action', 'continue', 'match', 'number', 'set']:
-                                            return { 'result': False, 'comment': "%s.%s.%s: rules key invalid" % (top_id, category, element) }
-                                    else:
-                                        if key not in ['prefix', 'ge', 'le']:
-                                            return { 'result': False, 'comment': "%s.%s.%s: rules key invalid" % (top_id, category, element) }
+            try:
+                policySchema().load(categories)
+            except ValidationError as error:
+                return { 'result': False, 'comment': '%s: invalid data' % top_id, 'out': error.messages }
 
-                elif category in ['aspath_lists', 'community_lists']:
-                    for element, elem_data in contents.items():
-                        if 'rules' not in elem_data:
-                            return { 'result': False, 'comment': "%s.%s.%s: only rules element should be set" % (top_id, category, element) }
-                        for rule in elem_data['rules']:
-                            # Needs to be a schema
-                            for key in rule.keys():
-                                if key not in ['action', 'description', 'regex']:
-                                    return { 'result': False, 'comment': "%s.%s.%s: rules key invalid" % (top_id, category, element) }
-
-                elif top_id.startswith('_') and category == 'roles':
-                    if not isinstance(contents, list):
-                        return { 'result': False, 'comment': "%s: roles must be a list" % top_id }
-
-                else:
-                    return { 'result': False, 'comment': "%s: invalid category found" % top_id}
-
-        return { 'result': True, 'comment': "%s - all checks passed" % top_id }
+        return { 'result': True, 'comment': "%s: all checks passed" % top_id }
---
+++ schema/policy.py
@@ -0,0 +1,86 @@
+
+from marshmallow import Schema, fields, validate, INCLUDE, ValidationError
+
+class policyAspathListSchema(Schema):
+    """ Not yet implemented. """
+
+
+class policyCommunityListSchema(Schema):
+    """ Not yet implemented. """
+
+
+class policyList4RulesSchema(Schema):
+    le = fields.Integer(validate=validate.Range(min=0, max=32))
+    ge = fields.Integer(validate=validate.Range(min=0, max=32))
+
+    prefix = fields.IPv4Interface(required=True)
+
+
+class policyList4NamesSchema(Schema):
+    rules = fields.List(fields.Nested(policyList4RulesSchema()), required=True, validate=validate.Length(min=1))
+
+
+class policyList6RulesSchema(Schema):
+    le = fields.Integer(validate=validate.Range(min=0, max=128))
+    ge = fields.Integer(validate=validate.Range(min=0, max=128))
+
+    prefix = fields.IPv6Interface(required=True)
+
+
+class policyList6NamesSchema(Schema):
+    rules = fields.List(fields.Nested(policyList6RulesSchema()), required=True, validate=validate.Length(min=1))
+
+
+class policyPrefixListSchema(Schema):
+    ipv4 = fields.Dict(keys=fields.String(required=True), values=fields.Nested(policyList4NamesSchema()))
+    ipv6 = fields.Dict(keys=fields.String(required=True), values=fields.Nested(policyList6NamesSchema()))
+
+
+class policyMapMatchSchema(Schema):
+    prefix_list    = fields.String()
+    community_list = fields.String()
+    as_path        = fields.String()
+
+    rpki = fields.String(validate=validate.OneOf(['notfound', 'valid', 'invalid']))
+
+
+class policyMapSetSchema(Schema):
+    local_pref = fields.Integer(validate=validate.Range(min=0, max=255))
+    next_hop   = fields.String()
+    origin     = fields.String()
+
+    community        = fields.String()
+    large_community  = fields.String()
+    as_path_exclude  = fields.Integer(validate=validate.Range(min=1,max=2**32))
+
+
+class policyMapRulesSchema(Schema):
+    class Meta:
+        include = {
+                'set':      fields.Nested(policyMapSetSchema()),
+                'continue': fields.Integer(validate=validate.Range(min=0, max=999)),
+                }
+
+    match  = fields.Nested(policyMapMatchSchema())
+    number = fields.Integer(required=True, validate=validate.Range(min=0, max=999))
+    action = fields.String(required=True, validate=validate.OneOf(['permit', 'deny']))
+
+
+class policyMapNamesSchema(Schema):
+    rules = fields.List(fields.Nested(policyMapRulesSchema()), required=True, validate=validate.Length(min=1))
+
+
+class policyRouteMapSchema(Schema):
+    ipv4 = fields.Dict(keys=fields.String(required=True), values=fields.Nested(policyMapNamesSchema()))
+    ipv6 = fields.Dict(keys=fields.String(required=True), values=fields.Nested(policyMapNamesSchema()))
+
+
+class policySchema(Schema):
+    prefix_lists = fields.Nested(policyPrefixListSchema())
+    route_maps   = fields.Nested(policyRouteMapSchema())
+
+    aspath_lists    = fields.Dict(keys=fields.String(required=True), values=fields.Nested(policyAspathListSchema()))
+    community_lists = fields.Dict(keys=fields.String(required=True), values=fields.Nested(policyCommunityListSchema()))
+
+    roles = fields.List(fields.String(), validate=validate.Length(min=1))
+
