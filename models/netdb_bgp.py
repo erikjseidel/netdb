817510ce66221994c7e00741cc0e39e1ad7107c4
Erik Seidel <erik@znsl.us>
2023-03-26 11:56:20 +0000

    marshmallow validation migration part 4

--- models/netdb_bgp.py
+++ models/netdb_bgp.py
@@ -1,5 +1,6 @@
 
 from .netdb_column      import netdbColumn
+from schema.bgp         import *
 
 class netdbBgp(netdbColumn):
 
@@ -21,34 +22,9 @@ class netdbBgp(netdbColumn):
                 if 'roles' not in categories.keys():
                     return { 'result': False, 'comment': "%s: roles required for shared config set" % top_id }
 
-            for category, contents in categories.items():
-                if category == 'address_family':
-                    for family, items in contents.items():
-                        if family not in ['ipv4', 'ipv6']:
-                            return { 'result': False, 'comment': "%s.%s: family must me either ipv4 or ipv6" % (top_id, category) }
+            try:
+                bgpSchema().load(categories)
+            except ValidationError as error:
+                return { 'result': False, 'comment': '%s: invalid data' % top_id, 'out': error.messages }
 
-                elif category in ['peer_groups', 'neighbors']:
-                    for element, elem_data in contents.items():
-                        if category == 'peer_groups':
-                            for i in elem_data.keys():
-                                if i not in ['family', 'type', 'remote_asn', 'source', 'password', 'multihop']:
-                                    return { 'result': False, 'comment': "%s.%s.%s: invalid peer_group key" % (top_id, category, element) }
-                        else:
-                            for i in elem_data.keys():
-                                if i not in ['peer_group', 'family', 'type', 'remote_asn']:
-                                    return { 'result': False, 'comment': "%s.%s.%s: invalid neighbor key" % (top_id, category, element) }
-
-                elif category == 'options':
-                    for option, value in contents.items():
-                        if not isinstance(value, str) and not isinstance(value, int):
-                            return { 'result': False, 'comment': "%s.%s.%s: invalid key or value" % (top_id, category, option) }
-
-                elif top_id.startswith('_') and category == 'roles':
-                    if not isinstance(contents, list):
-                        return { 'result': False, 'comment': "%s: roles must be a list" % top_id }
-
-                else:
-                    return { 'result': False, 'comment': "%s.%s: invalid category found" % (top_id, category) }
-
-        return { 'result': True, 'comment': "%s - all checks passed" % top_id }
-
+        return { 'result': True, 'comment': "%s: all checks passed" % top_id }
