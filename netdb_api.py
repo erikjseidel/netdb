8b2b7af2801c2dea6e957462fb2c83c5ab3aeaa3
Erik Seidel <erik@znsl.us>
2023-05-12 14:08:35 +0000

    cleanup

--- netdb_api.py
+++ netdb_api.py
@@ -43,15 +43,13 @@ def builder_route(column, top_id):
     if column not in netdb.COLUMNS:
         return ERR_INVALID_EP
 
-    status = 200
-
     # device and interface columns do not need config builders.
     if column not in ['device', 'interface']:
         response = builder.newBuilder(column, top_id).build()
     else:
         response = netdb.newColumn(column).filter(top_id).fetch()
 
-    return Response(response = json.dumps(response), status = status, mimetype = 'application/json')
+    return Response(response=json.dumps(response), status=status, mimetype='application/json')
 
 
 @app.route('/api/<column>/validate', methods=['POST', 'PUT'])
@@ -62,11 +60,9 @@ def validator_route(column):
     if not ( data := get_data(request) ):
         return ERR_INVALID_DATA
 
-    status = 200
-
     response = netdb.newColumn(column).set(data).validate()
 
-    return Response(response = json.dumps(response), status = status, mimetype = 'application/json')
+    return Response(response=json.dumps(response), status=status, mimetype='application/json')
 
 
 @app.route('/api/<column>/project', methods=['GET'])
@@ -77,11 +73,9 @@ def projector_route(column):
     if not ( data := get_data(request) ):
         return ERR_INVALID_DATA
 
-    status = 200
-
     response = netdb.newColumn(column).project(data).fetch()
 
-    return Response(response = json.dumps(response), status = status, mimetype = 'application/json')
+    return Response(response=json.dumps(response), status=200, mimetype='application/json')
 
 
 @app.route('/api/<column>', methods=['POST', 'PUT', 'DELETE'])
@@ -92,8 +86,6 @@ def alter_route(column, top_id):
     if not ( data := get_data(request) ):
         return ERR_INVALID_DATA
 
-    status = 200
-
     if request.method == 'POST':
         response = netdb.newColumn(column).set(data).save()
 
@@ -103,7 +95,7 @@ def alter_route(column, top_id):
     elif request.method == 'DELETE':
         response = netdb.newColumn(column).filter(data).delete()
 
-    return Response(response = json.dumps(response), status = status, mimetype = 'application/json')
+    return Response(response=json.dumps(response), status=200, mimetype='application/json')
 
 
 @app.route('/api/<column>',          methods=['GET'])
@@ -114,14 +106,12 @@ def fetcher_route(column, top_id = None):
 
     data = get_data(request)
 
-    status = 200
-
     if data:
         response = netdb.newColumn(column).filter(data).fetch()
     else:
         response = netdb.newColumn(column).filter(top_id).fetch()
 
-    return Response(response = json.dumps(response), status = status, mimetype = 'application/json')
+    return Response(response=json.dumps(response), status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
