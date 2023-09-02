f51a4090fa6289b57aabac24406a00f1229fa7cd
Erik Seidel <erik@znsl.us>
2023-09-02 12:22:40 +0000

    NetDB API in container initial support

--- netdb/config/defaults.py
+++
@@ -1,8 +0,0 @@
-# Mongod host and port
-MONGO_URL="mongodb://10.40.100.5:27017/"
-# Mongod DB name
-DB_NAME = 'netdb'
-# This requires a mongodb relica set
-TRANSACTIONS = True
-# Used for instances on proxy minions and only need reads.
-READ_ONLY = False
