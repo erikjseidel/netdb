b8d58c76f357b098b31dbc9e80e7109751f98200
Erik Seidel <erikjseidel@gmail.com>
2024-07-06 19:57:18 -0500

    Update lint.mk

--- lint.mk
+++ lint.mk
@@ -3,6 +3,6 @@ export LINT_IMAGE := erikjseidel/pylint-docker
 export APP_DIR := /netdb
 export PYLINTRC := /var/cache/.pylintrc
 
-.PHONY: link
+.PHONY: lint
 lint:
 	docker run --rm -v $(WORKING_DIR)/$(APP_DIR):$(APP_DIR)/ -v $(WORKING_DIR)/.pylintrc:$(PYLINTRC) -i $(LINT_IMAGE) pylint --rcfile=$(PYLINTRC) $(APP_DIR)
